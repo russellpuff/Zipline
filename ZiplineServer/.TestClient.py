a9f5bb187c672443b6bace96651bcb90c9f2ec43
Ryan Austin <ryan.rcaustin@gmail.com>
2023-04-20 11:48:26 -0400

    Refactored Unit Tests; Cleaned up test output

--- ZiplineServer/.TestClient.py
+++ ZiplineServer/.TestClient.py
@@ -1,7 +1,7 @@
 #######################################################################################################################
-# .TestClient.py
+## .TestClient.py
 #######################################################################################################################
-# A simple CLI client to quickly test server features.
+## A simple CLI client to quickly test server features.
 #######################################################################################################################
 
 import socket
@@ -10,22 +10,18 @@ host, port  = '137.184.241.2', 52525
 headerBytes = bytes([0x63, 0xC3, 0x1E, 0xE9, 0xF0, 0x58, 0x60, 0x1D])
 
 
+#######################################################################################################################
+## Supporting Functions
+#######################################################################################################################
+
 ####
-# makePackage(payload)
+# sendPackage(payload)
 ####
-# Support function to construct a byte sequence from a payload string.
+# Support function to construct a package, create a socket, send the package across it, and return the response
 ####
-def makePackage(payload):
+def sendPackage(payload):
     size = (4 + len(headerBytes) + len(bytes(payload, 'utf-8'))).to_bytes(4, 'big')
-    return size + headerBytes + bytes(payload, 'utf-8')
-
-
-####
-# sendPackage(package)
-####
-# Support function to create a socket, send a byte sequence across it, and return the response
-####
-def sendPackage(package):
+    package = size + headerBytes + bytes(payload, 'utf-8')
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
     sock.sendall(package)
@@ -45,13 +41,63 @@ def genResponse(package):
 
 
 ####
+# testResults(testname, response, expected)
+####
+# Support function to print the results of an individual test; returns 1 if test fails, 0 otherwise.
+####
+def testResults(testname, response, expected):
+    if response == expected:
+        print("[ ] " + testname)
+        return 0
+    else:
+        print("[X] " + testname)
+        print("    RESPONSE: {}\n    EXPECTED: {}".format(response,expected))
+        return 1
+
+
+####
+# printheader(testname)
+####
+# Support function to print a test header.
+####
+def printHeader(testname):
+    header = "TESTING [" + testname + "]"
+    print(header.ljust(120, '-'))
+    print()
+
+
+####
+# printFinalResults(testname, failures)
+####
+# Support function to print a summary of test results.
+def printFinalResults(testname, failures):
+    print()
+    testname = "[" + testname + "]: "
+    if failures == 0:
+        print(testname + "PASSES")
+    else:
+        print(testname + "FAILED {} TESTS".format(failures))
+    print(120 * '-')
+
+
+
+#######################################################################################################################
+## Test Functions
+#######################################################################################################################
+
+####
 # testAddNewFile()
 ####
 # Test the add_new_file command.
 ####
 def testAddNewFile():
-    print("TESTING [addNewFile(payload)]-----------------------------------------------------------------------------")
-    print("----------------------------------------------------------------------------------------------------------")
+    printHeader('addNewFile')
+    failures = 0
+    ## Test with Existing User, New File
+    ## Test with Existing User, Duplicate FIle
+    ## Test with Non-Existent User, New File
+    ## Test with Non-Existent user, Duplicate File
+    printFinalResults('addNewFile', failures)
 
 
 ####
@@ -60,8 +106,11 @@ def testAddNewFile():
 # Test the delete_file command.
 ####
 def testDeleteFile():
-    print("TESTING [deleteFile(payload)]-----------------------------------------------------------------------------")
-    print("----------------------------------------------------------------------------------------------------------")
+    printHeader('deleteFile')
+    failures = 0
+    ## Test with Existing File
+    ## Test with Non-Existent File
+    printFinalResults('deleteFile', failures)
 
 
 ####
@@ -70,32 +119,20 @@ def testDeleteFile():
 # Test the login_user command.
 ####
 def testLoginUser():
-    print("TESTING [loginUser(payload)]------------------------------------------------------------------------------")
+    printHeader('loginUser')
     failures = 0
+
     ## Test with New User
-    package = makePackage('{"Command":"login_user", "Username":"testuser1", "Password":"null", "LatestIP":"11.22.33.44:55123"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"login_user", "Username":"testuser1", "Password":"null", "LatestIP":"11.22.33.44:55123"}')
     expected = genResponse('STATUS_OK')
-    if response == expected:
-        print("[ ] New User Test")
-    else:
-        print("[X] New User Test")
-        failures += 1
+    failures += testResults('New User Test', response, expected)
+
     ## Test with Existing User, Different IP
-    package = makePackage('{"Command":"login_user", "Username":"testuser1", "Password":"null", "LatestIP":"55.66.77.88:55789"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"login_user", "Username":"testuser1", "Password":"null", "LatestIP":"55.66.77.88:55789"}')
     expected = genResponse('STATUS_OK')
-    if response == expected:
-        print("[ ] Existing User Test")
-    else:
-        print("[X] Existing User Test")
-        failures += 1
-    ## Final Report
-    if failures == 0:
-        print("[loginUser(payload)]: PASSES")
-    else:
-        print("[loginUser(payload)]: FAILED {} TESTS".format(failures))
-    print("----------------------------------------------------------------------------------------------------------")
+    failures += testResults('Existing User Test', response, expected)
+
+    printFinalResults('loginUser', failures)
     
 
 ####
@@ -104,41 +141,25 @@ def testLoginUser():
 # Test the logout_user command.
 ####
 def testLogoutUser():
-    print("TESTING [logoutUser(payload)]-----------------------------------------------------------------------------")
+    printHeader('logoutUser')
     failures = 0
+
     ## Test with Existing Online User
-    package = makePackage('{"Command":"logout_user", "Username":"testuser1"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"logout_user", "Username":"testuser1"}')
     expected = genResponse('STATUS_OK')
-    if response == expected:
-        print("[ ] Existing Online User Test")
-    else:
-        print("[X] Existing Online User Test")
-        failures += 1
+    failures += testResults('Existing Online User Test', response, expected)
+
     ## Test with Existing Offline User
-    package = makePackage('{"Command":"logout_user", "Username":"evan"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"logout_user", "Username":"evan"}')
     expected = genResponse('STATUS_OK')
-    if response == expected:
-        print("[ ] Existing Offline User Test")
-    else:
-        print("[X] Existing Offline User Test")
-        failures += 1
+    failures += testResults('Existing Offline User Test', response, expected)
+
     ## Testing with Non-Existent User
-    package = makePackage('{"Command":"logout_user", "Username":"nonexistentuser1"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"logout_user", "Username":"nonexistentuser1"}')
     expected = genResponse('STATUS_IGNORE')
-    if response == expected:
-        print("[ ] Non-Existent User Test")
-    else:
-        print("[X] Non-Existent User Test")
-        failures += 1
-    ## Final Report
-    if failures == 0:
-        print("[logoutUser(payload)]: PASSES")
-    else:
-        print("[logoutUser(payload)]: FAILED {} TESTS".format(failures))
-    print("----------------------------------------------------------------------------------------------------------")
+    failures += testResults('Non-Existent User Test', response, expected)
+
+    printFinalResults('logoutUser', failures)
 
 
 ####
@@ -147,41 +168,25 @@ def testLogoutUser():
 # Test the get_user_ip command.
 ####
 def testGetUserIP():
-    print("TESTING [getUserIP(payload)]------------------------------------------------------------------------------")
+    printHeader('getUserIP')
     failures = 0
+
     ## Test with Existing Online User
-    package = makePackage('{"Command":"get_user_ip", "TargetUser":"danielle"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"get_user_ip", "TargetUser":"danielle"}')
     expected = genResponse('''('{"LatestIP":"45.45.45.45:57123"}',)''')
-    if response == expected:
-        print("[ ] Existing Online User Test")
-    else:
-        print("[X] Existing Online User Test")
-        failures += 1
+    failures += testResults('Existing Online User Test', response, expected)
+
     ## Test with Existing Offline User
-    package = makePackage('{"Command":"get_user_ip", "TargetUser":"evan"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"get_user_ip", "TargetUser":"evan"}')
     expected = genResponse('STATUS_USER_OFFLINE')
-    if response == expected:
-        print("[ ] Existing Offline User Test")
-    else:
-        print("[X] Existing Offline User Test")
-        failures += 1
+    failures += testResults('Existing Offline User Test', response, expected)
+
     ## Test with Non-Existent User
-    package = makePackage('{"Command":"get_user_ip", "TargetUser":"nonexistentuser1"}')
-    response = sendPackage(package)
+    response = sendPackage('{"Command":"get_user_ip", "TargetUser":"nonexistentuser1"}')
     expected = genResponse('STATUS_USER_OFFLINE')
-    if response == expected:
-        print("[ ] Non-Existent User Test")
-    else:
-        print("[X] Non-Existent User Test")
-        failures += 1
-    ## Final Report
-    if failures == 0:
-        print("[getUserIP(payload)]: PASSES")
-    else:
-        print("[getUserIP(payload)]: FAILED {} TESTS".format(failures))
-    print("----------------------------------------------------------------------------------------------------------")
+    failures += testResults('Non-Existent User Test', response, expected)
+
+    printFinalResults('getUserIP', failures)
 
 
 ####
@@ -190,24 +195,30 @@ def testGetUserIP():
 # Test the verify_user_files command.
 ####
 def testVerifyUserFiles():
-    print("TESTING [verifyUserFiles(payload)]------------------------------------------------------------------------")
+    printHeader('verifyUserFiles')
     failures = 0
-    ## Test with One Unknown, One Missing, One OK
-    package = makePackage('{"Command":"verify_user_files", "Username":"barbara", "FileList":"garbage.jnk?mona-lisa.jpg"}')
-    response = sendPackage(package)
-    expected = genResponse("[{'garbage.jnk': 'STATUS_UNKNOWN_FILE'}, {'mona-lisa.jpg': 'STATUS_OK'}, {'sample.db': 'STATUS_MISSING_FILE'}]")
-    if response == expected:
-        print("[ ] One Unknown, One Missing, One OK Test")
-    else:
-        print("[X] One Unknown, One Missing, One OK Test")
-        failures += 1
-    ## Final Report
-    if failures == 0:
-        print("[verifyUserFiles(payload)]: PASSES")
-    else:
-        print("[verifyUserFiles(payload)]: FAILED {} TESTS".format(failures))
-    print("----------------------------------------------------------------------------------------------------------")
 
+    ## Test One Unknown File
+    response = sendPackage('{"Command":"verify_user_files","Username":"barbara","FileList":"garbage.jnk"}')
+    expected = genResponse("[{'garbage.jnk': 'STATUS_UNKNOWN_FILE'}, {'mona-lisa.jpg': 'STATUS_MISSING_FILE'}, {'sample.db': 'STATUS_MISSING_FILE'}]")
+    failures += testResults('Unknown File Test', response, expected)
+
+    ## Test One Missing File
+    response = sendPackage('{"Command":"verify_user_files","Username":"barbara","FileList":"sample.db"}')
+    expected = genResponse("[{'sample.db': 'STATUS_OK'}, {'mona-lisa.jpg': 'STATUS_MISSING_FILE'}]")
+    failures += testResults('Missing File Test', response, expected)
+
+    ## Test All OK Files
+    response = sendPackage('{"Command":"verify_user_files","Username":"barbara","FileList":"mona-lisa.jpg?sample.db"}')
+    expected = genResponse("[{'mona-lisa.jpg': 'STATUS_OK'}, {'sample.db': 'STATUS_OK'}]")
+    failures += testResults('All OK Files', response, expected)
+
+    ## Test with One Unknown, One Missing, One OK
+    response = sendPackage('{"Command":"verify_user_files", "Username":"barbara", "FileList":"garbage.jnk?mona-lisa.jpg"}')
+    expected = genResponse("[{'garbage.jnk': 'STATUS_UNKNOWN_FILE'}, {'mona-lisa.jpg': 'STATUS_OK'}, {'sample.db': 'STATUS_MISSING_FILE'}]")
+    failures += testResults('"One of Each" Test', response, expected)
+
+    printFinalResults('verifyUserFiles', failures)
 
 
 #######################################################################################################################
